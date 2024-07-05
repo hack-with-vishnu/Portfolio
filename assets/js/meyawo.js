e56c2e9933fb7be9f6f8ff7f08fcaddc43fa8960
v-dedsec <vv9569117@gmail.com>
2024-07-05 16:58:16 +0530

    completed

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -36,3 +36,74 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+
+
+  //Form validation
+  var nameError=document.getElementById('name-error');
+  var emailError=document.getElementById('email-error');
+  var subjectError=document.getElementById('subject-error');
+  var messageError=document.getElementById('message-error');
+  var submitError=document.getElementById('submit-error');
+ 
+  function validateName(){
+     var name = document.getElementById('name').value;
+     
+     if(name.length == 0){
+       nameError.innerHTML='Enter valid Name';
+       return false;
+     }
+     
+     if(!name.match(/^[A-Za-z]+ [A-Za-z]+$/)){
+       nameError.innerHTML = 'Enter Full Name';
+       return false;
+     }
+     nameError.innerHTML = ''
+     return true;
+  }
+ 
+ 
+  function validateEmail(){
+   var email=document.getElementById('mail').value;
+ 
+     if(email.length==0){
+       emailError.innerHTML='Enter Valid Email';
+       return false;
+     }
+ 
+     if(!email.match(/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/)){
+       emailError.innerHTML='Email Invalid';
+       return false;
+     }
+     emailError.innerHTML='';
+     return true;
+  }
+ 
+ 
+ 
+   
+ 
+   function validateMessage(){
+     var message=document.getElementById('comment').value;
+     var required=15;
+     var left=required-message.length;
+ 
+     if(left>0){
+       messageError.innerHTML=left+ ' More Characters Required';
+       return false;
+     }
+     messageError.innerHTML='';
+     return true;
+   }
+ 
+   function validateForm(){
+
+    if(!validateName() || !validateEmail() || !validateMessage()){
+        submitError.style.display=`block`;
+        submitError.innerHTML=`please fix error `;
+        setTimeout(function(){submitError.style.display = `none`;}, 3000);
+        return false;
+   
+ 
+   }
+}
